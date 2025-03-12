ca7fc8991ba5d9d93bfb43b300afa107865a51fa
Samp83 <sami.paris@supdevinci-edu.fr>
2025-03-12 09:58:47 +0100

    added swagger and cors

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,11 +2,14 @@ import express from "express";
 import authRoutes from "./routes/auth.routes";
 import productRoutes from "./routes/product.routes";
 import dotenv from "dotenv";
+import cors from "cors";
+
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.use(express.json());
 app.use("/auth", authRoutes);
