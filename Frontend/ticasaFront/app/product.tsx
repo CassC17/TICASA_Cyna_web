9594054a990657780b0669c871f207985137110f
Samp83 <138454436+Samp83@users.noreply.github.com>
2025-03-13 14:24:16 +0100

    Delete Frontend/ticasaFront directory

--- Frontend/ticasaFront/app/product.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Product() {
-  return (
-    <View className="flex-1 items-center justify-center p-4">
-      <Text className="text-2xl font-bold">Product Details</Text>
-    </View>
-  );
-}
--- Frontend/ticasaFront/global.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "expo-router/entry" {
-  export * from "expo-router";
-}
