a3a96f62a87cf45bd66bb09f690cae9012d278f2
Titinite <thibault.leray@supdevinci-edu.fr>
2025-03-28 15:04:04 +0100

    update footer & create contact page

---
+++ Frontend/ticasaFront/app/contact.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ContactPage() {
+
+  return (
+    <View className="flex-1 bg-gray-100 p-4">
+      <Text className="text-center text-red-500 mt-6">
+        Cette page est en cours de construction
+      </Text>
+    </View>
+  );
+}
--- Frontend/ticasaFront/components/Footer.tsx
+++ Frontend/ticasaFront/components/Footer.tsx
@@ -1,10 +1,22 @@
 import { View, Text } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import React from "react";
+import { Link } from "expo-router";
 
 export default function Footer() {
   return (
     <View className="w-full bg-gray-900 p-4 flex-row items-center justify-between space-x-8">
+      <View className="flex-row space-x-4">
+        <Link href="#">
+          <Text className="text-white text-sm">CGU</Text>
+        </Link>
+        <Link href="#">
+          <Text className="text-white text-sm">Mentions légales</Text>
+        </Link>
+        <Link href="/contact">
+          <Text className="text-white text-sm">Contact</Text>
+        </Link>
+      </View>
       <View className="flex-row space-x-4">
         <FontAwesome name="facebook" size={24} color="white" />
         <FontAwesome name="twitter" size={24} color="white" />
