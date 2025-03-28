ed5de86dbd97d8aa1a68fdcf26b6de911b7e9f7b
Titinite <thibault.leray@supdevinci-edu.fr>
2025-03-28 15:42:31 +0100

    form in pagecontact

--- Frontend/ticasaFront/app/contact.tsx
+++ Frontend/ticasaFront/app/contact.tsx
@@ -1,13 +1,53 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { Link } from "expo-router";
 
 export default function ContactPage() {
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
 
-  return (
-    <View className="flex-1 bg-gray-100 p-4">
-      <Text className="text-center text-red-500 mt-6">
-        Cette page est en cours de construction
-      </Text>
-    </View>
-  );
+    const handleSubmit = () => {
+        console.log(email, subject, message);
+    };
+
+    return (
+        <View className="flex-1 justify-center items-center px-5 bg-gray-100">
+            <Text className="text-2xl font-bold text-gray-800 mb-8">Contactez-nous</Text>
+            
+            <View className="w-full max-w-md">
+                <View>
+                    <Text className="mt-4 mb-1 text-black text-lg">Adresse mail</Text>
+                    <TextInput
+                        className="w-full h-12 border rounded-lg px-4 text-lg bg-white shadow"
+                        value={email}
+                        onChangeText={setEmail} 
+                    />
+                </View>
+                <View>
+                    <Text className="mt-4 mb-1 text-black text-lg">Sujet du message</Text>
+                    <TextInput 
+                        className="w-full h-12 border rounded-lg px-4 text-lg bg-white shadow"
+                        value={subject} 
+                        onChangeText={setSubject}
+                    />
+                </View>
+                <View>
+                    <Text className="mt-4 mb-1 text-black text-lg">Message</Text>
+                    <TextInput
+                        className="w-full h-40 border rounded-lg px-4 py-2 text-lg bg-white shadow"
+                        value={message}
+                        onChangeText={setMessage}
+                        multiline={true}
+                        textAlignVertical="top"
+                    />
+                </View>
+
+                <TouchableOpacity onPress={handleSubmit} className="w-full h-12 bg-blue-500 justify-center items-center mt-5 rounded-lg shadow-lg">
+                    <Text className="text-white text-lg font-bold">Envoyer</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
